afd5f05f16cb658aa71f733fd249cf06a6f0a8b7
Jorge-Marrero <57073594+Jorge-Marrero@users.noreply.github.com>
2021-01-29 11:53:47 +0000

    Examen HPDS

---
+++ src/main/java/RomanNumerals.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RomanNumerals {
+    public static Map<Character, Integer> numbers = new HashMap<Character, Integer>();
+    static{
+        numbers.put('M', 1000);
+        numbers.put('D', 500);
+        numbers.put('C', 100);
+        numbers.put('L', 50);
+        numbers.put('X', 10);
+        numbers.put('V', 5);
+        numbers.put('I', 1);
+    }
+
+    enum characters{
+        M(1000), CM(900), D(500),CD(400),
+        C(100), XC(90), L(50), XL(40),
+        X(10), IX(9), V(5), IV(4), I(1);
+
+        private int value;
+
+        characters(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static List<characters> getReverseSortedValues() {
+            return Arrays.stream(values()).sorted(Comparator.comparing((characters e) -> e.value).reversed()).collect(Collectors.toList());
+        }
+    }
+    public static int romanToArabic(String roman){
+        int arabic = 0;
+        int current;
+        int after;
+        for(int i = roman.length()-1 ; i >= 0; i--){
+            if(!checkNumber(roman.charAt(i))) return -1;
+            if(i > 0){
+                current = numbers.get(roman.charAt(i));
+                after = numbers.get(roman.charAt(i-1));
+                if(current > after){
+                    arabic += (current - after);
+                    i--;
+                }else{
+                    arabic += current;
+                }
+            }else{
+                arabic += numbers.get(roman.charAt(i));
+            }
+        }
+        return arabic;
+    }
+
+    public static String arabicToRoman(int arabic){
+        if (arabic < 1) return "Numero no valido";
+        List<characters> arabicList = characters.getReverseSortedValues();
+        StringBuilder roman = new StringBuilder();
+        int i = 0;
+        while (i < arabicList.size() && (arabic > 0)){
+            characters current = arabicList.get(i);
+            if(current.getValue() <= arabic){
+                roman.append(current.name());
+                arabic -= current.getValue();
+            }else{
+                i++;
+            }
+        }
+        return roman.toString();
+    }
+
+    public static boolean checkNumber(char num){
+        return num == 'M' ||
+                num == 'D' ||
+                num == 'C' ||
+                num == 'L' ||
+                num == 'X' ||
+                num == 'V' ||
+                num == 'I';
+    }
+}
---
+++ src/test/java/RomanNumeralTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RomanNumeralTest {
+
+    @Test
+    public void testArabicToRoman(){
+        assertTrue(RomanNumerals.arabicToRoman((16)).equals("XVI"));
+        assertTrue(RomanNumerals.arabicToRoman((4)).equals("IV"));
+        assertTrue(RomanNumerals.arabicToRoman((1666)).equals("MDCLXVI"));
+        assertTrue(RomanNumerals.arabicToRoman((1990)).equals("MCMXC"));
+        assertTrue(RomanNumerals.arabicToRoman((-50000)).equals("Numero no valido"));
+    }
+
+    @Test
+    public void testRomanToArabic(){
+        assertTrue(RomanNumerals.romanToArabic("XVI") == 16);
+        assertTrue(RomanNumerals.romanToArabic("IV") == 4);
+        assertTrue(RomanNumerals.romanToArabic("MDCLXVI") == 1666);
+        assertTrue(RomanNumerals.romanToArabic("MCMXC") == 1990);
+        assertTrue(RomanNumerals.romanToArabic("Esto no es un numero") == -1);
+    }
+}
